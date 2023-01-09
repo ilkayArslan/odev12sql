e3ed2c8adb58928220e602dc9ba26cfe1decd533
İlkay <108187198+ilkayArslan@users.noreply.github.com>
2023-01-09 14:35:58 +0300

    Add files via upload
    isdone

---
+++ odev12.sql
@@ -0,0 +1,29 @@
+/*
+SELECT COUNT(*) FROM film
+WHERE length > 
+(
+SELECT ROUND(AVG(length)) FROM film
+);
+
+SELECT  COUNT(*) FROM film 
+WHERE rental_rate = 
+(
+SELECT MAX(rental_rate) FROM film
+);
+
+SELECT * FROM film 
+WHERE rental_rate = 
+(
+SELECT MIN(rental_rate) FROM film
+)
+AND
+replacement_cost = 
+(
+	SELECT MIN(replacement_cost) FROM film
+);
+*/
+SELECT customer_id, (COUNT(*)) AS rentmount FROM payment
+GROUP BY customer_id
+ORDER BY rentmount DESC;
+
+
